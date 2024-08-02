955a4650bf8ff1fc9db4bcfd5d467e0f637fe443
kreativesahara <technologieslensmen@gmail.com>
2024-08-03 01:18:52 +0300

    added an external styling library by Tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/Footer'
-import './App.css'
+
 
 function App  () {
   return (
     <> 
       <Navbar/>   
       <main>
-        <h2>This is an animations Website</h2>
-        <p>My profile about animating and my interests in animation will enter here</p>
+        <h2 className=''>This is an animations Website</h2>
+        <p className='text-3xl font-bold underline bg-orange-700'>My profile about animating and my interests in animation will enter here</p>
       </main>
       <Footer/>
     </>
