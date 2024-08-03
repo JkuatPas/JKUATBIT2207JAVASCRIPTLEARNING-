1e44fdbefbf997c06706dab9aa407fb0ad669914
kreativesahara <technologieslensmen@gmail.com>
2024-08-03 22:07:41 +0300

    Added styling to home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,31 @@ import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/Footer'
 import HeaderImage from './assets/animateone.gif'
-
-
-
+import './Control.css'
 function App  () {
   return (
     < > 
       <Navbar/>   
-      <main className='bg-indigo-600 w-full h-screen'>
-        <img src={HeaderImage} alt=""/>
-        <h2 >This is an animations Website</h2>
-        <p className=''>My profile about animating and my interests in animation will enter here</p>
+      <header className='pt-16 justify-center'>
+        <section className='text-center'>
+          <div className='title'>
+          <span className='text-3xl font-bold text-slate-100 mt-20'>Kreativ_Saharaa</span> 
+          <span>We are an animations studio</span>
+          </div>
+        </section>
+      </header>
+      <h3>Some Animations</h3>
+      <main className='bg-indigo-600 w-full'>
+        <section>
+          <img src={HeaderImage} alt="" />
+          <h2>This is a page with animations</h2>
+          <p>Some animations will be implemented here</p>
+        </section>
+        <section>
+          <img src={HeaderImage} alt="" />
+          <h2>This is a page with more animations</h2>
+          <p>Even more animations will be implemented here</p>
+        </section>
       </main>
       <Footer/>
     </>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return ( 
     <div className='shadow-md w-full fixed top-0 left-0'>
       <nav className='md:flex md:justify-between md:items-center bg-white text-indigo-600 py-4 md:px-10 px-6 text-2xl font-bold'>
-        <span>Mwongera,s Animate Studio</span>
+        <span>Kreativ_Saharaa...</span>
         <ul className='md:flex md:items-center md:pb-0 pb-12 space-x-4'>
             <li>
                 <Link to='/home'>Home</Link>
