cfa4aa6c90e997a74ff48f33744f4b03cfcc4705
kreativesahara <technologieslensmen@gmail.com>
2024-08-05 17:01:06 +0300

    Added: Form to the contacts list for collaborators to teach our

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -15,14 +15,17 @@ const Contact = () => {
     defaultValues: {
       firstname: "",
       lastname: "",
-      email: ""
+      email: "",
     }
   });
 
-  console.log(watch("email"));
+  console.log(watch( "email"));
   return (
     <div>
       <Navbar  />
+      <section className='mt-16 text-6xl font-bold '>
+        <h1>Contact Us</h1>
+      </section>
       <main className='bg-indigo-600 w-full h-screen mt-16'>
         <form className='w-1/3 mx-auto mt-20 bg-slate-300 p-10 rounded-lg'
           onSubmit={handleSubmit((data) => {
@@ -31,15 +34,13 @@ const Contact = () => {
         >
           <label>First Name</label>
           <input {...register("firstname", { required: true, maxLength: 10 })} />
-          {errors.firstname && <p>Your Firstname is required</p>}
+          {errors.firstname && <p className='invalid'>Your Firstname is required</p>}
           <label>Last Name</label>
           <input {...register("lastname", { required: true, maxLength: 10 })} />
-          {errors.lastname && <p>You lastname is required</p>}
+          {errors.lastname && <p className='invalid'>You lastname is required</p>}
           <label>Email Address</label>
-          <input
-            {...register("email", { required: true, maxLength: 10 })}
-          />
-          {errors.email && <p>Please Enter Email</p>}
+          <input {...register("email", { required: true, maxLength: 10 })} />
+          {errors.email && <p className='invalid'>Please Enter Email</p>}
           <input type="submit" />
         </form>
       </main>
