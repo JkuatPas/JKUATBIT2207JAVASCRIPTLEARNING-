1b4f1b7a475ac4f494305c8f3e364f9c9a7e2f86
lucas-naomi <135626014+lucas-naomi@users.noreply.github.com>
2024-08-21 16:50:44 +0300

    Implemented form feedback feature

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('#feedback_form')
+
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    alert('Thank you for your feedback! Come again😊')
+
+})
